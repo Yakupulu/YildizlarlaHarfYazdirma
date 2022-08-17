3e2aeb56ea5827e19e1d3dac828d321c8ee1a9f8
Yakup Ulu <yakup1720@gmail.com>
2022-08-17 23:27:11 +0300

    A ve B harfi yazdirma

---
+++ src/Aharfi.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Aharfi {
+    public static void main(String[] args) {
+
+        String[][] letter=new String[5][4];
+
+        for (int i=0;i< letter.length;i++) {
+            for (int j=0;j<letter[i].length;j++) {
+                if (i==0||i==2){
+                    System.out.print(" * ");
+                } else if (j==0||j==3) {
+                    System.out.print(" * ");
+                }else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
